50bc1aabe8d0e71da50941de0dfd0e547ae34ede
dylan delancie <dylandelancie@students.berkeley.net>
2017-11-07 11:38:11 -0800

    OH YEAH

--- Horoscope.js
+++ Horoscope.js
@@ -1,1 +1,27 @@
-.documentElementbyid().value;
+function onSubmit(){
+    getInfo();
+
+
+}
+
+function getInfo(){
+    var month = document.getElementById("month").value;
+    var day = document.getElementById("day").value;
+    var name = document.getElementById("name").value;
+
+}
+var signs = ["Aquarius","Pisces","Aries","Taurus","Gemini","Cancer","Leo","Virgo","Libra","Scorpio","Sagittarius","Capricorn"]
+signs[determineSign(month,day)]
+function determineSign(month,day){
+    var sign = "";
+    if((month == 1 && day >= 20) || (month == 2 && day <= 18)){
+        sign = 0;
+    }
+    if((month == 2 && day >= 19) || )
+}
+
+
+
+
+
+
